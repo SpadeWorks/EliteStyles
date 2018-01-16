dd47f13b3fb45429176d0646973d45bf2980ee5e
AkashBadhe <shreyas.bhave@asiapac.local>
2018-01-16 18:56:36 +0530

    Added fiels for DashBoard.

---
+++ js/wizard.js
@@ -0,0 +1,99 @@
+$(document).ready(function () {
+
+    var navListItems = $('div.setup-panel div a'),
+            allWells = $('.setup-content'),
+            allNextBtn = $('.nextBtn');
+
+    allWells.hide();
+
+    navListItems.click(function (e) {
+        e.preventDefault();
+        var $target = $($(this).attr('href')),
+                $item = $(this);
+
+        if (!$item.hasClass('disabled')) {
+            navListItems.removeClass('btn-primary').addClass('btn-default');
+            $item.addClass('btn-primary');
+            allWells.hide();
+            $target.show();
+            $target.find('input:eq(0)').focus();
+        }
+    });
+
+    allNextBtn.click(function(){
+        var curStep = $(this).closest(".setup-content"),
+            curStepBtn = curStep.attr("id"),
+            nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
+            curInputs = curStep.find("input[type='text'],input[type='url']"),
+            isValid = true;
+
+        $(".form-group").removeClass("has-error");
+        for(var i=0; i<curInputs.length; i++){
+            if (!curInputs[i].validity.valid){
+                isValid = false;
+                $(curInputs[i]).closest(".form-group").addClass("has-error");
+            }
+        }
+
+        if (isValid)
+            nextStepWizard.removeAttr('disabled').trigger('click');
+    });
+
+    $('div.setup-panel div a.btn-primary').trigger('click');
+
+
+
+
+
+
+
+$("button.nextBtn").click(function(){
+ if ($('.test_cases a').hasClass('btn-primary')) {
+       
+    $(".car").addClass("middle")
+    
+        }
+
+
+});
+
+
+$("button.nextBtn").click(function(){
+ if ($('.servay a').hasClass('btn-primary')) {
+    $(".car").removeClass("middle")   
+    $(".car").addClass("last")
+    
+        }
+
+
+});
+
+
+//datepicker script starts here
+
+ var date_input=$('input[name="date"]'); //our date input has the name "date"
+      var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+      var options={
+        format: 'mm/dd/yyyy',
+        container: container,
+        todayHighlight: true,
+        autoclose: true,
+      };
+      date_input.datepicker(options);
+
+
+
+$(".datepicker").removeClass("datepicker-orient-bottom");
+$(".datepicker").addClass("datepicker-orient-top");
+
+      //datepicke ends here
+
+
+
+
+});
+
+
+
+
+
